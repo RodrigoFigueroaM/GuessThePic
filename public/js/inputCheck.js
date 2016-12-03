be65a3cb90610db98744278211f58695f1ec13ef
saralip <saralip@csu.fullerton.edu>
2016-12-02 18:02:54 -0800

    # of letter check

--- public/js/inputCheck.js
+++ public/js/inputCheck.js
@@ -1,16 +1,31 @@
 var answer = "Google"
+var numOfClicks = 0;
 var main = function(){
 
   buildBut(answer);
+  $('#answer').prop("disabled", true);
 
   $('.EachChar').on("click", function(){
+    numOfClicks++;
     console.log(this.innerHTML);
     $('.answerChar').append(this.innerHTML);
     this.disabled = true;
     this.style.backgroundColor= "#e0b3ff";
+    if (numOfClicks == answer.length)
+    {
+      $('#answer').prop("disabled", false);
+    }
+
+  });
+  
+  $('#clear').on("click", function(){
+    $('.answerChar').empty();
+    $('.EachChar').prop("disabled", false);
+
   });
 
   $('#answer').on("click", function(){
+
     console.log(document.getElementById("testChar").innerHTML);
     $('#newAnswer').append(document.getElementById("testChar").innerHTML);
   });
@@ -27,3 +42,5 @@ function buildBut(wordIn){
         $('.inChar').append(  '<button class="EachChar ui violet button">'+wordIn[i]+'</button>');
   }
 }
+
+
